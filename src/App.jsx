4ae62457e77613b7c5283d69496a7d192fa0bcf3
Namah Agrawal <namahagrawal85@gmail.com>
2025-06-11 15:38:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Calculator from "./components/Calculator"
+
+function App() {
+  
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <span className="mb-10 text-3xl font-bold">Calculator App</span>
+      <Calculator/>
+    </div> 
+  )
+}
+
+export default App
